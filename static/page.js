b62f6303026c958bab8192c8adcba34f10fc050b
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-03-29 20:49:04 -0400

    Update page.js

--- static/page.js
+++ static/page.js
@@ -593,7 +593,14 @@ module.exports = function (req, res, url) {
         <li><a class="gtm-logout" href="https://ga.vyond.com/logoff">Log Out</a></li>
     </ul>
 </li>
-	<body style="margin:0px" onload="hideHeader()">${toObjectString(attrs, params)
+<body style="margin:0px" onload="hideHeader()">${toObjectString(attrs, params)
 		}</body>${stuff.pages[url.pathname] || ''}`);
+	<footer class="site-footer">
+    <div class="container">
+        Vyond™ is a trademark of GoAnimate. Inc. ©2018 GoAnimate. Inc. <a href="https://www.vyond.com/terms">Terms of Service</a> | <a href="https://www.vyond.com/privacy">Privacy Policy</a> | <a href="https://www.vyond.com/cookies">Cookies Policy</a>
+    </div>
+</footer>
+
 	return true;
 }
+
